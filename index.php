96008374df882642c5a02524cc7a4ecce3427409
Stiven-souce <stiviz2003@gmail.com>
2026-08-26 18:46:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/includes/config.php';
+require_once('includes/config.php');
 require_once __DIR__ . '/includes/productos.php';
 require_once __DIR__ . '/includes/carrito.php';
 
